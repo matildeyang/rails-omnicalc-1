b76a945f82678647afac64af6e310a16583b6b8f
matildeyang <91755817+matildeyang@users.noreply.github.com>
2023-10-25 00:18:55 +0000

    square root done

--- app/controllers/calculate_controller.rb
+++ app/controllers/calculate_controller.rb
@@ -3,7 +3,19 @@ class CalculateController < ApplicationController
     render({:template => "my_templates/new_square"})
   end 
 
+  def square_results 
+    @num = params.fetch("num")
+    @result = @num.to_f ** 2 
+    render({:template => "my_templates/square_results"})
+  end 
+
   def new_square_root
     render({:template => "my_templates/new_square_root"})
   end
+
+  def square_root_results 
+    @num = params.fetch("num")
+    @result = @num.to_f ** 0.5 
+    render({:template => "my_templates/square_root_results"})
+  end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get("/square/new", { :controller => "calculate", :action => "new_square" })
+  get("/square/results", { :controller => "calculate", :action => "square_results" })
   get("/square_root/new", { :controller => "calculate", :action => "new_square_root" })
+  get("/square_root/results", { :controller => "calculate", :action => "square_root_results" })
 end
